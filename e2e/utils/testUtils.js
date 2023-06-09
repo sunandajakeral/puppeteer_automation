c0c14b11dbb2b13c0af8affdc388f2863c7da8fc
Sunanda Jakeral <jakeralbsunanda@gmail.com>
2023-06-09 16:54:25 +0200

    added the launchBrowser util

---
+++ e2e/utils/testUtils.js
@@ -0,0 +1,14 @@
+const puppeteer = require("puppeteer");
+
+async function launchBrowser() {
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath:
+      "node_modules/chromium/lib/chromium/chrome-mac/Chromium.app/Contents/MacOS/Chromium", // Replace with the path to the Chromium executable
+  });
+  const page = await browser.newPage();
+
+  return [browser, page];
+}
+
+module.exports = { launchBrowser };
