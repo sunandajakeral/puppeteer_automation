15f65072158d92fc994a98ca2527f56a6f58cedc
Sunanda Jakeral <jakeralbsunanda@gmail.com>
2023-06-09 16:52:26 +0200

    added the locators required for new user registration test

--- e2e/locators.js
+++ e2e/locators.js
@@ -6,3 +6,14 @@ export const loginForm = "#login-form";
 export const loginEmailInput = "#loginEmail";
 export const loginPasswordInput = "#loginPassword";
 export const loginButton = "#login-submit";
+
+// Register a new user
+export const joinNowButton = '#registerAccount';
+export const salutationDropDown = '#salutation';
+export const firstNameInput = '#firstName';
+export const lastNameInput = '#lastName';
+export const emailInput = '#email';
+export const passwordInput = '#password';
+export const repeatPassword2nput = '#password2';
+export const registerForm = '#register-form';
+export const registerButton = '#register-submit';
