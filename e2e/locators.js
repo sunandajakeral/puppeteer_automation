b3bbc731fca812563969788679cc79d136852815
Sunanda Jakeral <jakeralbsunanda@gmail.com>
2023-06-09 16:45:10 +0200

    added the locators required for login test

---
+++ e2e/locators.js
@@ -0,0 +1,8 @@
+// Login with existing account Page
+export const headerMenuContainer= '#headerMenuContainer'
+export const acceptAllButton = '[data-accept-action="all"]';
+export const loginLink = 'a[href="/login"]';
+export const loginForm = "#login-form";
+export const loginEmailInput = "#loginEmail";
+export const loginPasswordInput = "#loginPassword";
+export const loginButton = "#login-submit";
