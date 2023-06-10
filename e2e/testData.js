426d4749678bddcd5e4bd531656474319a9fa3f6
Sunanda Jakeral <jakeralbsunanda@gmail.com>
2023-06-10 19:24:18 +0200

    testdata added

---
+++ e2e/testData.js
@@ -0,0 +1,5 @@
+export const SALUTATION = "Frau";
+export const FIRST_NAME = "firstname";
+export const LAST_NAME = "lastname";
+export const EMAIL = `firstlast${Math.floor(Math.random() * 100)}@gmail.com`;
+export const PASSWORD = `Password@${Math.floor(Math.random() * 1000)}`;
