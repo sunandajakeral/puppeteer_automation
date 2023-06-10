3b4afe68996d82604886c1720f998ba2812d2221
Sunanda Jakeral <jakeralbsunanda@gmail.com>
2023-06-10 19:41:46 +0200

    moved pageobjects of homepage to different file

--- e2e/pages/LoginPage.js
+++ e2e/pages/LoginPage.js
@@ -5,31 +5,6 @@ export default class LoginPage {
     this.page = page;
   }
 
-  async navigateToLoginPage(appUrl) {
-    // Navigate to the login page
-    await this.page.goto(appUrl);
-  }
-
-  async isHomePageDisplayed(){
-    await this.page.$eval(locators.headerMenuContainer, (element) => {
-      return window.getComputedStyle(element).getPropertyValue('display') !== 'none';
-    });
-  }
-
-  async acceptAllCookies() {
-    // Accepts the cookies if the window exists
-    await this.page.$eval(locators.acceptAllButton, (element) => {
-      if (element) {
-        element.click();
-      }
-    });
-  }
-
-  async clickLoginLink() {
-    await this.page.waitForSelector(locators.loginLink);
-    await this.page.click(locators.loginLink);
-  }
-
   async enterCredentials(username, password) {
     await this.page.waitForSelector(locators.loginForm);
 
@@ -41,6 +16,10 @@ export default class LoginPage {
   async clickLoginButton() {
     // Click the login button
     await this.page.click(locators.loginButton);
+  }
+
+  async waitForLoginForm() {
+    await this.page.waitForSelector(locators.loginForm);
   }
 
   async clickJoinNowButton() {
