8adc2bb95e86ea5a048528e6f7df997a45e20b10
Sunanda Jakeral <jakeralbsunanda@gmail.com>
2023-06-10 19:28:43 +0200

    locators are added

--- e2e/locators.js
+++ e2e/locators.js
@@ -2,18 +2,20 @@
 export const headerMenuContainer= '#headerMenuContainer'
 export const acceptAllButton = '[data-accept-action="all"]';
 export const loginLink = 'a[href="/login"]';
+export const customerAccountLink = 'a[href="/kundenkonto"]';
 export const loginForm = "#login-form";
 export const loginEmailInput = "#loginEmail";
 export const loginPasswordInput = "#loginPassword";
 export const loginButton = "#login-submit";
 
 // Register a new user
-export const joinNowButton = '#registerAccount';
+export const joinNowButton = '[data-testid="registerAccount"]';
 export const salutationDropDown = '#salutation';
 export const firstNameInput = '#firstName';
 export const lastNameInput = '#lastName';
 export const emailInput = '#email';
 export const passwordInput = '#password';
-export const repeatPassword2nput = '#password2';
+export const repeatPasswordInput = '#password2';
 export const registerForm = '#register-form';
 export const registerButton = '#register-submit';
+export const termsConditionsButton = '[data-testid="nullLabel"]';
