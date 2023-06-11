dc77f95111da83d3de01f5cbc352c8be7a5ad933
Sunanda Jakeral <jakeralbsunanda@gmail.com>
2023-06-11 10:41:26 +0200

    added error messages

--- e2e/testData.js
+++ e2e/testData.js
@@ -3,3 +3,10 @@ export const FIRST_NAME = "firstname";
 export const LAST_NAME = "lastname";
 export const EMAIL = `firstlast${Math.floor(Math.random() * 100)}@gmail.com`;
 export const PASSWORD = `Password@${Math.floor(Math.random() * 1000)}`;
+export const LOGIN_WITHOUT_ACCOUNT_ERROR =
+  "Benutzername nicht gefunden oder Passwort falsch.";
+export const LOGIN_INVALID_EMAIL_ERROR =
+  "Bitte geben Sie eine gültige E-Mail-Adresse ein";
+export const LOGIN_WITH_EMPTY_EMAIL_ERROR = "Bitte geben Sie Ihre E-Mail-Adresse ein";
+export const PASSWORD_ERROR =
+  "Bitte verwenden Sie ein Passwort von mindestes 8 Zeichen mit mindestens einem Kleinbuchstaben, einem Großbuchstaben, einer Zahl und einem Sonderzeichen.";
